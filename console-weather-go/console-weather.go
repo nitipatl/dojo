bd44e09d43dd99a28d146da860ddea37e98de000
Nitipat L <iamsvz@gmail.com>
2018-04-03 09:08:18 +0700

    add array to code

--- console-weather-go/console-weather.go
+++ console-weather-go/console-weather.go
@@ -1,8 +1,19 @@
 package weather
 
 func digit(input int) string {
-	output := "  "
-	output += " |"
-	output += " |"
+
+	top := []string{"   ", "   "}
+	mid := []string{"   ", "  |"}
+	bot := []string{"   ", "  |"}
+	output := ""
+	if input == 2 {
+		output += " _ \n"
+		output += " _|\n"
+		output += "|_"
+	} else {
+		output += top[1] + "\n"
+		output += mid[1] + "\n"
+		output += bot[1]
+	}
 	return output
 }
--- console-weather-go/console-weather_test.go
+++ console-weather-go/console-weather_test.go
@@ -3,11 +3,19 @@ package weather
 import "testing"
 
 func Test_input_1_digitDisplay_1(t *testing.T) {
-	output := "  "
-	output += " |"
-	output += " |"
+	output := "   \n"
+	output += "  |\n"
+	output += "  |"
 	if digit(1) != output {
 		t.Error("actual ", digit(1))
 	}
+}
 
+func Test_input_2_digitDisplay_2(t *testing.T) {
+	output := " _ \n"
+	output += " _|\n"
+	output += "|_"
+	if digit(2) != output {
+		t.Error("actual ", digit(2))
+	}
 }
